cc1e315e3db57395ded63ebc512324102eefbf51
blackdeqth <sravankumaryedhoti@gmail.com>
2016-09-17 13:24:02 -0500

    added subtraction

--- add.go
+++ add.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main(){
- fmt.Println(add(3,4))
+ fmt.Println("Addition", add(3,4))
+ fmt.Println("Subtraction", subtract(3,4))
 }
 
 func add(a int, b int) int {
     return a + b;
+}
+
+func subtract(a int, b int) int {
+    return a - b;
 }
